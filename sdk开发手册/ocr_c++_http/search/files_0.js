97af2a767381070db345513b73a67e928c2baf3d
liuyongxin <liuyongxin@baller-tech.com>
2019-11-29 19:44:10 +0800

    更新OCR的开发手册

--- sdk开发手册/ocr_c++_http/search/files_0.js
+++ sdk开发手册/ocr_c++_http/search/files_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['baller_5fcommon_2eh',['baller_common.h',['../baller__common_8h.html',1,'']]],
-  ['baller_5ferrors_2eh',['baller_errors.h',['../baller__errors_8h.html',1,'']]],
-  ['baller_5focr_2eh',['baller_ocr.h',['../baller__ocr_8h.html',1,'']]],
-  ['baller_5ftypes_2eh',['baller_types.h',['../baller__types_8h.html',1,'']]]
+  ['baller_5fcommon_2eh_125',['baller_common.h',['../baller__common_8h.html',1,'']]],
+  ['baller_5ferrors_2eh_126',['baller_errors.h',['../baller__errors_8h.html',1,'']]],
+  ['baller_5focr_2eh_127',['baller_ocr.h',['../baller__ocr_8h.html',1,'']]],
+  ['baller_5ftypes_2eh_128',['baller_types.h',['../baller__types_8h.html',1,'']]]
 ];
--- sdk开发手册/ocr_c++_http/search/typedefs_0.js
+++ sdk开发手册/ocr_c++_http/search/typedefs_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['baller_5fsession_5fid',['baller_session_id',['../baller__types_8h.html#a670ed74b65cdf44fd56a17d80590d5dd',1,'baller_types.h']]]
+  ['baller_5fsession_5fid_136',['baller_session_id',['../baller__types_8h.html#a670ed74b65cdf44fd56a17d80590d5dd',1,'baller_types.h']]]
 ];
